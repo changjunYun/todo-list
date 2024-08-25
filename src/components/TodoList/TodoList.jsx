38d4cf6bb03f7572f85450eb0273b8553c04224a
changjun <yuncj5925@gmail.com>
2024-08-25 21:49:58 +0900

    줄정리

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList(){
-    
     const [todos, setTodos] = useState([
         { id: '123', text: '장보기', status: 'active' },
         { id: '124', text: '공부하기', status: 'active' },
     ]);
+    
     const handleAdd = (todo) => {
         // 새로운 todo를 todos에 업데이트 해줘야 한다.
         console.log(todo);
