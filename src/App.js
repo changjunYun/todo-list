60a80507079b35e5bc9869e1104a09e71d2efb09
changjun <yuncj5925@gmail.com>
2024-08-26 12:27:22 +0900

    리스트 필터기능 추가 (#7)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import TodoList from './components/TodoList/TodoList';
 import './App.css';
 import Header from './components/Header/Header';
+import { useState } from 'react';
+
+const filters = ['all', 'active', 'completed'];
 
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return ( 
   <>
-    <Header />
-    <TodoList />
+    <Header 
+    filters={filters} 
+    filter={filter} 
+    // onFilterChange={(filter) => setFilter(fliter)}
+    onFilterChange={setFilter}
+    />
+    <TodoList filter={filter} />
   </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({filters, filter, onFilterChange}) {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <ui>
+                {filters.map((value, index) => 
+                <li key={index}>
+                    <button onClick={() => onFilterChange(value)}>{value}</button>
+                </li>)}
+            </ui>
+        </header>
     );
 }
 
